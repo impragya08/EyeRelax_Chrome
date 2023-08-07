0bf457b02e26ecfd764c4713f7bc4ea4891f7ddc
Pragya Srivastava <84717393+impragya08@users.noreply.github.com>
2023-08-07 14:45:51 +0530

    First

---
+++ popup.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // This function is executed when the popup HTML has finished loading.
+  
+    const dismissButton = document.getElementById('dismissButton');
+    // Get a reference to the "Dismiss" button in the popup by its element ID.
+  
+    dismissButton.addEventListener('click', function () {
+      // Add a click event listener to the "Dismiss" button.
+  
+      window.close();
+      // When the button is clicked, this function will be executed, and it will close the popup using `window.close()`.
+    });
+  });
